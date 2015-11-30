7373c3bf10ba1e3cd4e8952761c7fb4924eb3217
Elvis DOHMATOB <elvis.dohmatob@inria.fr>
2015-11-30 08:45:21 +0100

    implemented banner

--- 2048.py
+++ 2048.py
@@ -9,40 +9,63 @@ except ImportError:
     print("Oops! Looks like you haven't installed the numpy library.")
     print("Bye!")
     sys.exit(1)
+from _get_arrow import GetArrow
 
 # raw_input doesn't exist in Python3 (renamed to "input")
 input_grabber = input if sys.version_info.major == 3 else raw_input
+
+banner = """
+  ________________________________________________________________
+< 2048 game dedicated to the young dr. alex jr., @ parietal _team_ >
+  ----------------------------------------------------------------
+         \   ^__^
+          \  (oo)\_______
+             (__)\       )\/\
+
+                 ||----w |
+                 ||     ||
+"""
 
 
 class Game2048(object):
 
     _SIZES = [2, 4, 8]  # allowed sizes for the game grid
 
-    def __init__(self, size=None, grid=None, random_state=None):
+    def __init__(self, size=None, grid=None, random_state=None, mode='arrows'):
         # misc
-        if size is None:
-            while not size in self._SIZES:
+        if not mode in ["arrows", "letters"]:
+            raise ValueError("Invalid mode: %s" % mode)
+        self.mode = mode
+        self.size = size
+        self.grid = grid
+        self.random_state = random_state
+        self.load_batteries()
+
+    def load_batteries(self):
+        """Some serious conf business."""
+        print(banner)
+        self.rng_ = np.random.RandomState(self.random_state)
+
+        # get size of grid
+        if self.size is None:
+            while not self.size in self._SIZES:
                 try:
-                    size = int(input_grabber(
+                    self.size = int(input_grabber(
                         ("<> Enter game configuration (size in %s, etc.):"
                          " ") % self._SIZES))
                 except ValueError:
                     continue
-        self.size = size
-        self.grid = grid
-        self.random_state = random_state
-        self.rng_ = np.random.RandomState(random_state)
 
         # make grid
-        if grid is None:
-            self.grid_ = np.zeros((size, size), dtype=int)
-            indices = list(np.ndindex((size, size)))
+        if self.grid is None:
+            self.grid_ = np.zeros((self.size, self.size), dtype=int)
+            indices = list(np.ndindex((self.size, self.size)))
             support = self.rng_.choice(range(len(indices)), 2)
             self.grid_[indices[support[0]][0], indices[support[0]][1]] = 2
             self.grid_[indices[support[0]][1],
                        indices[support[1]][1]] = self.rng_.choice([2, 4])
         else:
-            self.grid_ = np.array(grid).copy()
+            self.grid_ = np.array(self.grid).copy()
 
     def get_params(self):
         """Get all parameters of class instance."""
@@ -52,6 +75,11 @@ class Game2048(object):
     def get_move(self):
         """Get next move from input sensor (screen, etc.)."""
         print(self)
+        if self.mode == "arrows":
+            mv = GetArrow()()
+            if not mv is None:
+                return dict(up="I", down="K", left="J", right="L")[mv]
+            return None
         mv = "X"
         while not mv in ["I", "J", "K", "L"]:
             mv = input_grabber(("<> Enter direction for movement "
@@ -169,11 +197,13 @@ class Game2048(object):
 
     def play(self):
         """Play game."""
-        print("=" * 80)
-        print("2048 : play the 2048 game")
-        print(("The player enters direction for movement : I = up, J = left,"
-               " K = down, L = right"))
+        if self.mode == "arrows":
+            print("Use arrows (up, down, left, right) to play game")
+        else:
+            print(("The player enters direction for movement : I = up, J = "
+                   "left, K = down, L = right"))
         print("Note : enter an empty line to stop the game")
+        print("=" * 80)
         while True:
             mv = self.get_move()
             if mv is None:
---
+++ _get_arrow.py
@@ -0,0 +1,40 @@
+import sys
+import tty
+import termios
+
+
+class GetArrow(object):
+    def _getarrow(self):
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            arrow = sys.stdin.read(3)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return arrow
+
+    def __call__(self):
+        while True:
+            arrow = self._getarrow()
+            if arrow == "":
+                return None
+            while True:
+                if arrow != '':
+                    break
+            if arrow == '\x1b[A':
+                return "up"
+            elif arrow == '\x1b[B':
+                return "down"
+            elif arrow == '\x1b[C':
+                return "right"
+            elif arrow == '\x1b[D':
+                return "left"
+            else:
+                print "Not an arrow arrow!"
+                return None
+
+if __name__ == '__main__':
+    gk = GetArrow()
+    for _ in range(1):
+        print gk()
